78eaf8bcc935d2551ad60824ea97c9fa565413ae
pratiktamang <pratiktamang13@gmail.com>
2023-04-22 08:59:36 +0100

    false state can be saved

--- app/models/mentor_profile.rb
+++ app/models/mentor_profile.rb
@@ -5,7 +5,7 @@ class MentorProfile < ApplicationRecord
   validates :ruby_start_year, presence: true
   validates :country, presence: true
   validates :city, presence: true
-  validates :previous_mentoring, presence: true
+  validates :previous_mentoring, inclusion: {in: [true, false]}
   validates :motivation, presence: true
   validates :communication_preference, presence: true
   validates :industry_expertise, presence: true
