de6b7cfc0be222eef6285da82ee9ea1d11b5c3eb
pratiktamang <pratiktamang13@gmail.com>
2023-04-22 08:23:47 +0100

    redirect to dashboard after create

--- app/controllers/mentors/registrations_controller.rb
+++ app/controllers/mentors/registrations_controller.rb
@@ -12,9 +12,9 @@ class Mentors::RegistrationsController < Devise::RegistrationsController
   # end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
   # def edit
@@ -53,9 +53,9 @@ class Mentors::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    mentors_dashboard_path
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
