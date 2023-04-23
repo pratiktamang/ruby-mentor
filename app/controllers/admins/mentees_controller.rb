b8386d055bf18adeecf9147b6441f286a1b53d3d
pratiktamang <pratiktamang13@gmail.com>
2023-04-23 20:15:23 +0100

    update admin/mentee/index view to show all mentee profile answers

--- app/controllers/admins/mentees_controller.rb
+++ app/controllers/admins/mentees_controller.rb
@@ -1,5 +1,6 @@
 class Admins::MenteesController < ApplicationController
   def index
     @mentees = Mentee.all
+    @mentee_headers = MenteeProfile.column_names.reject { |c| c == "id" || c == "mentee_id" || c == "created_at" || c == "updated_at" }
   end
 end
