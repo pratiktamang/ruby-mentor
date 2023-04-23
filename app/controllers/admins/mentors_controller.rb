becf0b148ad9e1f2dcbcfa8e89189dfb0bcf6e3b
pratiktamang <pratiktamang13@gmail.com>
2023-04-23 20:49:56 +0100

    increase width to show all data on adminview

--- app/controllers/admins/mentors_controller.rb
+++ app/controllers/admins/mentors_controller.rb
@@ -3,5 +3,6 @@ class Admins::MentorsController < ApplicationController
 
   def index
     @mentors = Mentor.all
+    @mentor_headers = MentorProfile.column_names.reject { |c| c == "id" || c == "mentor_id" || c == "created_at" || c == "updated_at" }
   end
 end
